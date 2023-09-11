a2916f42d07c38efae3e9fb1faa7a5caf21b0bca
Firoz Rahman Sabuz <everythingkst@gmail.com>
2023-09-11 11:56:06 +0600

    Rotate proxies

--- test.py
+++ test.py
@@ -1,9 +1,37 @@
-import pandas
+import requests
+import random
+proxy_list = []
+with open('raw_proxy_list.txt') as f :
+    for line in f:
+        proxy_list.append(line.strip())
+    working_proxies = []
+    for i in proxy_list:
+        print('IP that is being checked: ', i)
+        try:
+            proxy = {
+                "http":"http://"+i,
+                "https":"http://"+i
+            }
+            res = requests.get('http://example.org/', proxies=proxy)
+            working_proxies.append(i)
+        except:
+            pass
 
-dt = pandas.read_csv('email.csv').to_dict()
-email = dt['Email']
-i = 0
-while i < len(dt['Email']):
-    l = v.verify(email[i])
-    print(l['address'][1] + ' ... ' + str(l['deliverable']))
-    i += 1
+print("Active IPs: ", working_proxies)
+
+for i in range(3):
+    random_ip = random.choice(working_proxies)
+    print("Random IP selected: ", random_ip)
+
+    try:
+        proxy = {
+            "http": "http://" + random_ip,
+            "https": "http://" + random_ip
+        }
+        res = requests.get('http://ip.jsontest.com/', proxies=proxy)
+        print(f"Request received from following IP:\n {res.text}")
+    except:
+        pass
+
+
+
