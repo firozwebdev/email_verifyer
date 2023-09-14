26a9f847a1e9573fed4ad184c388cc26fd3efc92
Sabuz <everythingkst@gmail.com>
2023-09-14 20:02:25 +0600

    html template added

--- sending_mail/sender.py
+++ sending_mail/sender.py
@@ -1,19 +1,14 @@
-import smtplib, socket
+import smtplib, socket, ssl
 import time, random
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 import pandas
 
-message = """From: From Person %s  
-    To: To Person %s  
 
-    MIME-Version:1.0  
-    Content-type:text/html  
+# Define the HTML document
+with open('templates/file.html', 'r') as f:
+    html = f.read()
 
-
-    Subject: Sending SMTP e-mail   
-
-    <h3>Python SMTP</h3>  
-    <strong>This is a test e-mail message.</strong>  
-    """
 
 
 ds = pandas.read_csv('senders_list.csv')
@@ -23,21 +18,53 @@ password = ds['password']
 receiver_email = dr['email']
 i = 0
 failure_counter = 0
+date_str = pandas.Timestamp.today().strftime('%Y-%m-%d')
+
 try:
-    while i < len(receiver_email):
-        sender_mail = 'tryan6745@gmail.com'
-        sender_password = 'papxetftgyewdtar'
-        receivers_mail = receiver_email[i]
-        smtpObj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-        smtpObj.login(sender_mail, sender_password)
-        # smtpObj = smtplib.SMTP('localhost')
-        smtpObj.sendmail(sender_mail, receiver_email[i], message)  # single sender to multiple receivers
-        print(f"Successfully sent email to {receiver_email[i]}")
-        number = range(1, 6)
-        time.sleep(random.choice(number))
-        i += 1
+    sender_mail = 'tryan6745@gmail.com'
+    sender_password = 'papxetftgyewdtar'
+    receiver_mail = 'everythingkst@gmail.com'
+    # Create a MIMEMultipart class, and set up the From, To, Subject fields
+    email_message = MIMEMultipart()
+    email_message['From'] = sender_mail
+    email_message['To'] = receiver_mail
+    email_message['Subject'] = f'Report email - {date_str}'
+
+    # Attach the html doc defined earlier, as a MIMEText html content type to the MIME message
+
+    email_message.attach(MIMEText(html, "html"))
+    # Convert it as a string
+    email_string = email_message.as_string()
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_mail, sender_password)
+        server.sendmail(sender_mail, receiver_mail, email_string)
+    print("mail sent successfully")
+
 except socket.error as e:
     failure_counter += 1
     print(f"Error: failure to send email: {failure_counter}")
     print(f"Error: unable to send email: {e}")
     pass
+
+# Below code are working with single sender to multiple receivers
+# try:
+
+#     while i < len(receiver_email):
+#         sender_mail = 'tryan6745@gmail.com'
+#         sender_password = 'papxetftgyewdtar'
+#         receivers_mail = receiver_email[i]
+#         smtpObj = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+#         smtpObj.login(sender_mail, sender_password)
+#         # smtpObj = smtplib.SMTP('localhost')
+#         smtpObj.sendmail(sender_mail, receiver_email[i], message)  # single sender to multiple receivers
+#         print(f"Successfully sent email to {receiver_email[i]}")
+#         number = range(1, 6)
+#         time.sleep(random.choice(number))
+#         i += 1
+# except socket.error as e:
+#     failure_counter += 1
+#     print(f"Error: failure to send email: {failure_counter}")
+#     print(f"Error: unable to send email: {e}")
+#     pass
